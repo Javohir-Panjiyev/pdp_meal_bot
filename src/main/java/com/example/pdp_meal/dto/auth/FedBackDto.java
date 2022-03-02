beec2d9029ed789e469310ef22bd5e908aa1b5c4
Pjavohir <95359250+Pjavohir@users.noreply.github.com>
2022-03-02 18:29:57 +0500

    Fedbacktype

---
+++ src/main/java/com/example/pdp_meal/dto/auth/FedBackDto.java
@@ -0,0 +1,11 @@
+package com.example.pdp_meal.dto.auth;
+
+/**
+ * @author Panjiyev Javohir, ср 18:28. 02.03.2022
+ */
+public class FedBackDto {
+
+    private String message;
+    private String type;
+
+}
