3ac754c4d6033590d98dca1a148ec7e9c39d4c94
Botirov Najmiddin <95509783+NajmiddinBG@users.noreply.github.com>
2022-03-03 16:11:46 +0500

    Change Red data

---
+++ src/main/java/com/example/pdp_meal/dto/feedback/FeedBackCreateDto.java
@@ -0,0 +1,21 @@
+package com.example.pdp_meal.dto.feedback;
+
+import com.example.pdp_meal.dto.BaseDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author Panjiyev Javohir, ср 21:08. 02.03.2022
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeedBackCreateDto implements BaseDto {
+
+    private String message;
+
+    private String type;
+}
